810d9b90a2a41a9ec6cf570b67937c8f66cdd35d
Chris Nady <nady.christopher@gmail.com>
2017-08-26 23:50:47 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -7,16 +7,22 @@ function getIt() {
 
 function frameIt() {
   $('img').on('load', function(){
-    $('img').className = '.tasty';
+    window.$('img').className = 'tasty';
   });
 }
 
 function submitIt() {
-
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.');
+  });
 }
 
 function pressIt() {
-
+  $('form').on('keydown', function(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+});
 }
 
 $(document).ready(function(){
