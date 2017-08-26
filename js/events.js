e7b237d600bc32143e9c39d37bb69b05447555c2
Chris Nady <nady.christopher@gmail.com>
2017-08-26 23:48:26 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
 //define functions here
+function getIt() {
+  $('p').on("click", function(){
+    alert("Hey!");
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $('img').className = '.tasty';
+  });
+}
+
+function submitIt() {
+
+}
+
+function pressIt() {
+
+}
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+submitIt();
+pressIt();
 });
